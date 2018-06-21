1acdb197d44f7dd676b4c22a1222398f710f051f
yang.deng <dyc5810911@126.com>
2018-06-21 18:02:50 +0800

    修改界面

--- src/view/login/js/login.js
+++ src/view/login/js/login.js
@@ -1,16 +1,18 @@
+/**
+ * @version : v1.0.0
+ * @date    : 2018.06.21
+ **/
 'use strict'
-
 const ipcRenderer = require('electron').ipcRenderer;
 const User = require('../../../user/user.js');
 const log4js = require('../../../common/log.js');
 const $ = require('jquery');
 const logger = log4js.getLogger();
+
 async function login() {
 	try {
 		let username = $('#username').val();
 		let password = $('#password').val();
-		username = 'admin';
-		password = '111111';
 		let user = new User();
 		let obj = await user.login(username,password);
 		let body = JSON.parse(obj.body);
